871b1ab2f439b45631cc91ca439b0b78a126964e
RedlineBotGt <camposcacho@gmail.com>
2026-09-25 01:18:34 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1165,12 +1165,13 @@ async function showEmbedPreviewAndConfirm(interaction: any) {
     new ButtonBuilder().setCustomId('embed_cancel_btn').setLabel('CANCELAR').setStyle(ButtonStyle.Danger)
   );
 
-      if (interaction.isModalSubmit()) {
-        await interaction.reply({ content: resumenInfo, components: [actionRow], ephemeral: true });
-    } else {
-        await interaction.update({ content: resumenInfo, components: [actionRow], ephemeral: true });
+              if (interaction.isModalSubmit()) {
+            await interaction.reply({ content: resumenInfo, components: [actionRow], ephemeral: true });
+        } else {
+            await interaction.update({ content: resumenInfo, components: [actionRow], ephemeral: true });
+        }
     }
-} // <--- Este cierra el client.on de arriba
+}); // Cierra el escuchador principal
 
 // Iniciar sesión
 const token = process.env.DISCORD_TOKEN;
@@ -1178,5 +1179,4 @@ if (!token) {
     console.error('❌ ERROR: No se ha encontrado el token.');
 } else {
     client.login(token);
-}
-  
+    }
