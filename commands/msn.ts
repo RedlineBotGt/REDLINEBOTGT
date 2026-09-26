7c66d4994598ae5fbaea1a1ab09598a832bf32e4
RedlineBotGt <camposcacho@gmail.com>
2026-09-26 18:59:13 +0200

    Add channel selection menu to msn command

--- commands/msn.ts
+++ commands/msn.ts
@@ -1,5 +1,17 @@
-import { SlashCommandBuilder } from 'discord.js';
+import {
+  SlashCommandBuilder,
+  ChannelSelectMenuBuilder,
+  ChannelType,
+} from 'discord.js';
 
 export const msnCommand = new SlashCommandBuilder()
   .setName('msn')
   .setDescription('Enviar un mensaje desde REDLINE');
+
+export const msnChannelSelect = new ChannelSelectMenuBuilder()
+  .setCustomId('msn_select_channel')
+  .setPlaceholder('Selecciona el canal de destino')
+  .setChannelTypes(
+    ChannelType.GuildText,
+    ChannelType.GuildAnnouncement
+  );
